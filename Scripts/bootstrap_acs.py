4108d1fb3549d5605ec683adf9633dbc2ae5477d
JF Brouillette <jfbrou@stanford.edu>
2022-03-18 20:38:34 -0700

    Update bootstrap_acs.py.

--- Scripts/bootstrap_acs.py
+++ Scripts/bootstrap_acs.py
@@ -15,24 +15,24 @@ from functions import *
 # Set the Sherlock data directory
 data = '/scratch/users/jfbrou/Dignity'
 
-# Define a function to read the data by year
-def year_chunk(file, chunksize=1e6):
-    iterator = pd.read_csv(file, iterator=True, chunksize=chunksize)
-    chunk = pd.DataFrame()
-    for df in iterator:
-        unique_years = np.sort(df.year.unique())
-        if len(unique_years) == 1:
-            chunk = chunk.append(df, ignore_index=True)
-        else:
-            chunk = chunk.append(df.loc[df.year == unique_years[0], :], ignore_index=True)
-            yield chunk
-            chunk = pd.DataFrame()
-            chunk = chunk.append(df.loc[df.year == unique_years[1], :], ignore_index=True)
-    yield chunk
-chunks = year_chunk(os.path.join(data, 'bootstrap_acs.csv'), chunksize=1e6)
-
 # Define a function to calculate ACS consumption and leisure statistics across bootstrap samples
 def bootstrap(b):
+    # Define a function to read the data by year
+    def year_chunk(file, chunksize=1e6):
+        iterator = pd.read_csv(file, iterator=True, chunksize=chunksize)
+        chunk = pd.DataFrame()
+        for df in iterator:
+            unique_years = np.sort(df.year.unique())
+            if len(unique_years) == 1:
+                chunk = chunk.append(df, ignore_index=True)
+            else:
+                chunk = chunk.append(df.loc[df.year == unique_years[0], :], ignore_index=True)
+                yield chunk
+                chunk = pd.DataFrame()
+                chunk = chunk.append(df.loc[df.year == unique_years[1], :], ignore_index=True)
+        yield chunk
+    chunks = year_chunk(os.path.join(data, 'bootstrap_acs.csv'), chunksize=1e6)
+    
     # Load and process the ACS data year by year
     df_b = pd.DataFrame()
     for chunk in chunks:
