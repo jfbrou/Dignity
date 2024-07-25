a10e2673269bc7e7f5d34d2d43a90aa5cb4c3f3c
Jean-Felix Brouillette <jfbrou@stanford.edu>
2024-07-25 13:44:02 -0400

    Create acs.sh.

---
+++ Programs/Bash/acs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH -p hns
+#SBATCH --job-name=acs
+#SBATCH --error=/home/users/jfbrou/Dignity_bash/errors_%x_%j.err
+#SBATCH --output=/home/users/jfbrou/Dignity_bash/output_%x_%j.out
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=50G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jfbrou@stanford.edu
+
+cd /home/users/jfbrou/Dignity/Programs
+ml python
+python3 acs.py
