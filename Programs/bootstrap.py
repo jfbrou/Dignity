4ae79e25f62264875f59c49ae5d99912dcab4fef
Jean-Felix Brouillette <jfbrou@stanford.edu>
2024-07-29 15:07:11 -0400

    Update bootstrap.py.

--- Programs/bootstrap.py
+++ Programs/bootstrap.py
@@ -17,7 +17,7 @@ data = '/scratch/users/jfbrou/Dignity'
 # Append all bootstrap samples in a single data frame
 dignity_cex_bootstrap = pd.DataFrame()
 dignity_cps_bootstrap = pd.DataFrame()
-for b in range(1000):
+for b in range(1, 1000 + 1, 1):
     df_cex = pd.read_csv(os.path.join(data, 'dignity_cex_bootstrap_' + str(b) + '.csv'))
     df_cps = pd.read_csv(os.path.join(data, 'dignity_cps_bootstrap_' + str(b) + '.csv'))
     dignity_cex_bootstrap = pd.concat([dignity_cex_bootstrap, df_cex], ignore_index=True)
@@ -164,7 +164,7 @@ for sample in samples:
 
 # Append all bootstrap samples in a single data frame
 cew_bootstrap = pd.DataFrame()
-for b in range(1000):
+for b in range(1, 1000 + 1, 1):
     df = pd.read_csv(os.path.join(data, 'cew_bootstrap_ ' + str(b) + '.csv'))
     cew_bootstrap = pd.concat([cew_bootstrap, df], ignore_index=True)
     del df
