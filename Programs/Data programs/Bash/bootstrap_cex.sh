6456b6dc7c211ff52aa5e0a8a1e40dd1b8b2ed0a
Jean-Felix Brouillette <jfbrou@stanford.edu>
2024-12-07 16:05:27 -0500

    Update several files.

--- Programs/Data programs/Bash/bootstrap_cex.sh
+++ Programs/Data programs/Bash/bootstrap_cex.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 #SBATCH --job-name=bootstrap_cex
-#SBATCH --error=/home/jfbrou/Dignity/Programs/Data programs/Bash/Error/errors_%x_%j.err
-#SBATCH --output=/home/jfbrou/Dignity/Programs/Data programs/Bash/Output/output_%x_%j.out
+#SBATCH --error=/home/jfbrou/Dignity_bash/errors_%x_%j.err
+#SBATCH --output=/home/jfbrou/Dignity_bash/output_%x_%j.out
 #SBATCH --time=1-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
